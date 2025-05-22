1039ea717341fecbf20c172c750837c89fc72066
Adea Piperku <ap64975@ubt-uni.net>
2025-05-22 11:55:13 +0200

    App Menu dashboard

---
+++ web-app/src/components/dashboard/AppMenu.js
@@ -0,0 +1,73 @@
+import React from "react";
+import PersonIcon from "@material-ui/icons/Person";
+import {
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  useTheme,
+} from "@material-ui/core";
+import { useNavigate } from "react-router-dom";
+import AppMenuItem from "./AppMenuItem";
+import ExitToAppIcon from "@material-ui/icons/ExitToApp";
+import DashboardIcon from "@material-ui/icons/Dashboard";
+import SimpleBar from "simplebar-react";
+import "simplebar/dist/simplebar.min.css";
+import SupervisorAccountIcon from '@material-ui/icons/SupervisorAccount';
+import useUser from "../../hooks/useUser";
+
+const appMenuItems = [
+  {
+    name: "Dashboard",
+    Icon: DashboardIcon,
+    link: "/admin/dashboard",
+  },
+  {
+    name: "Admins",
+    Icon: SupervisorAccountIcon,
+    link: "/admin/admins",
+  },
+  {
+    name: "Customers",
+    Icon: PersonIcon,
+    link: "/admin/customers",
+  },
+  {
+    name: "Favorites",
+    Icon: PersonIcon,
+    link: "/admin/favorites",
+  },
+];
+
+export default function AppMenu({}) {
+  const theme = useTheme();
+  const navigate = useNavigate();
+  const { setUser } = useUser();
+
+  function handleLogOut() {
+    setUser(null)
+    localStorage.removeItem('user')
+    navigate("/client/home", { replace: true });
+  }
+
+  return (
+    <SimpleBar
+      style={{
+        height: `calc(100% - ${theme.toolbarHeight}px)`,
+        overflowX: "hidden",
+      }}
+    >
+      <List component="nav">
+        {appMenuItems.map((item, index) => (
+          <AppMenuItem {...item} key={index} />
+        ))}
+        <ListItem key={-1} onClick={handleLogOut} button>
+          <ListItemIcon>
+            <ExitToAppIcon />
+          </ListItemIcon>
+          <ListItemText>Log out</ListItemText>
+        </ListItem>
+      </List>
+    </SimpleBar>
+  );
+}
